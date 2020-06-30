d63785637c738dbfc22216baf91a31ddc42c1854
Sunil <sunil@infiproton.com>
2020-06-30 20:05:07 +0530

    first commit

---
+++ src/test/java/com/example/project/googleService/GoogleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.googleService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
