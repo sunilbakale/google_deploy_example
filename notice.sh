a4227c3180fdf5fa38e0d6f3523408aaeea9b8b6
Sunil <sunil@infiproton.com>
2020-06-30 20:47:17 +0530

    modified in docker file

---
+++ notice.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo '
+                   ***** NOTICE *****
+Alternative official `gradle` images, including multiple tagged versions across
+multiple platforms, can be found at https://hub.docker.com/_/gradle.
+                ***** END OF NOTICE *****
+'
+
+/usr/bin/gradle "$@"
